71404a32910fcca9289fedb33503437e3353826b
unknown <thaissp96@gmail..com>
2021-11-05 20:27:11 -0300

    exercicio loja de games

---
+++ Spring/MinhaLojaDeGames/src/main/java/br/org/generation/minhalojadegames/model/ProdutoModel.java
@@ -0,0 +1,66 @@
+package br.org.generation.minhalojadegames.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table (name= "tb_produtos")
+@Valid
+
+public class ProdutoModel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_produto;
+	
+	@NotBlank
+	private String Titulo;
+	
+	@NotBlank
+	private BigDecimal Preco;
+	
+	@NotBlank
+	private int Quantidade;
+
+	public long getId_produto() {
+		return id_produto;
+	}
+
+	public void setId_produto(long id_produto) {
+		this.id_produto = id_produto;
+	}
+
+	public String getTitulo() {
+		return Titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.Titulo = titulo;
+	}
+
+	public BigDecimal getPreco() {
+		return Preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.Preco = preco;
+	}
+
+	public int getQuantidade() {
+		return Quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.Quantidade = quantidade;
+	}
+	
+		
+	
+
+}
