47f531cf438c8bac29daa0beeeff6ee4c84bae9a
unknown <thaissp96@gmail..com>
2021-11-11 17:09:59 -0300

    spring security

--- atvblogpessoal/src/main/java/br/org/generation/atvblogpessoal/repository/TemaRepository.java
+++ atvblogpessoal/src/main/java/br/org/generation/atvblogpessoal/repository/TemaRepository.java
@@ -3,9 +3,10 @@ package br.org.generation.atvblogpessoal.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.org.generation.atvblogpessoal.model.Tema;
-
+@Repository
 public interface TemaRepository extends JpaRepository<Tema, Long> {
 	
 	public List <Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
